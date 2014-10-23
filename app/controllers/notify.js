6608cfcaeedb1558c4b30f88bcab0ef190ed858e
Pedro Ribeiro <pcelta.ribeiro@gmail.com>
2014-10-23 19:26:53 -0200

    added body-parser dependence

--- app/controllers/notify.js
+++ app/controllers/notify.js
@@ -2,15 +2,19 @@
 module.exports = {
     notify: function(req, res) {
 
-    	res.set({
-		  'Content-Type': 'json/application'		
-		})
+        var parameters = req.body;
 
-    	responseData = {
-    		"message": "received message",
-    		"body": req.body.message+"ds"
-    	}
+        res.set({
+          'Content-Type': 'json/application'
+        });
 
-        res.send('notify', responseData);
+        if (JSON.stringify(parameters) === '{}') {
+            responseData = {
+                "message": "request no body"
+            }
+            res.status(400).send(responseData);
+        }
+
+    	res.status(201).send();
     }
 };
